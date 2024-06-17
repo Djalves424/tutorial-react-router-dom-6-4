6fb810c35d8cda083bedc3bc3706b7d5f02c541e
Djalves424 <djalves424@gmail.com>
2024-06-17 14:24:55 -0300

    Links without full page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import './App.css'
+import { Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  
   return (
-<h1>Bookkeeper!</h1>
-    )
+    <nav
+      style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem",
+      }}
+    >
+      <Link to="/invoices">Invoices</Link> |{" "}
+      <Link to="/expenses">Expenses</Link>
+    </nav>
+  );
 }
 
-export default App
+export default App;
