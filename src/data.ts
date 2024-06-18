b139fc1d23afdf295f40113077aad8f04222a6b0
Djalves424 <djalves424@gmail.com>
2024-06-18 16:31:34 -0300

    Navigating Programmatically

--- src/data.ts
+++ src/data.ts
@@ -40,3 +40,9 @@ export function getInvoice(number: number) {
         (invoice) => invoice.number === number
     );
 }
+
+export function deleteInvoice(number: any) {
+    invoices = invoices.filter(
+        (invoice) => invoice.number !== number
+    );
+}
--- src/routes/invoices/Invoice/index.tsx
+++ src/routes/invoices/Invoice/index.tsx
@@ -1,8 +1,9 @@
-import { useParams } from "react-router-dom";
-import { getInvoice } from "../../../data";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteInvoice, getInvoice } from "../../../data";
 
 export default function Invoice() {
   const params = useParams();
+  const navigate = useNavigate();
 
   const invoice = getInvoice(Number(params.invoiceId));
 
@@ -15,6 +16,16 @@ export default function Invoice() {
             {invoice.name}: {invoice.number}
           </p>
           <p>Due Date: {invoice.due}</p>
+          <p>
+            <button
+              onClick={() => {
+                deleteInvoice(invoice.number);
+                navigate("/invoices" + location.search);
+              }}
+            >
+              Delete
+            </button>
+          </p>
         </main>
       )}
     </>
